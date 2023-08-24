ae1bce657d044172a565d069b6da6f38a730657f
Andrei Nikolov <andrei.nikolov@gmail.com>
2023-08-24 15:30:56 +0300

    fetching posts data and passing it to addPost()

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const postsURL = "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json";
+const dateOptions = {
+    day: "numeric",
+    month: "long", 
+    year: "numeric"
+};
+const dateLocale = "en-GB";
+const defaultCategory = "Articles"; 
+
 function addPost(category, imageUrl, postTitle, authorName, authorProfileUrl, datePosted, postUrl) {
     document.getElementById("posts-container").innerHTML += `
             <div class="col-4">
@@ -18,14 +27,61 @@ function addPost(category, imageUrl, postTitle, authorName, authorProfileUrl, da
 `;    
 }
 
-addPost("CLOUD AND SERVER", "https://assets.ubuntu.com/v1/36f1139e-Design-and-Web-Team-Blog.jpg", 
-    "Travel, CLIs, and sticky notes: Lilyana\u2019s life as a Canonical UX designer", "James Nunns", 
-    "https://ubuntu.com/", "27 November 2018", "https://ubuntu.com/");
+// order of preference: group, category, post_tag
+function choseCategoryToShow(wpTermData) {
+    let group = null;
+    let category = null;
+    let post_tag = null; //for display purposes in the card header we will use just one of the tags as failover if no group or category are set
 
-addPost("CLOUD AND SERVER", "https://assets.ubuntu.com/v1/36f1139e-Design-and-Web-Team-Blog.jpg", 
-    "Travel, CLIs, and sticky notes: Lilyana\u2019s life as a Canonical UX designer", "James Nunns", 
-    "https://ubuntu.com/", "27 November 2018", "https://ubuntu.com/");
+    console.log(wpTermData);
 
-addPost("CLOUD AND SERVER", "https://assets.ubuntu.com/v1/36f1139e-Design-and-Web-Team-Blog.jpg", 
-    "Travel, CLIs, and sticky notes: Lilyana\u2019s life as a Canonical UX designer", "James Nunns", 
-    "https://ubuntu.com/", "27 November 2018", "https://ubuntu.com/");
+    for (let i = 0; i < wpTermData.length; i++) {
+        if (wpTermData[i].length == 0) continue; 
+        switch (wpTermData[i][0].taxonomy) {
+            case "category":
+                category = wpTermData[i][0].name;
+                break;
+            case "group":
+                group = wpTermData[i][0].name;
+                break;
+            case "post_tag":
+                post_tag = wpTermData[i][0].name;
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (group !== null) return group;
+    if (category !== null) return category;
+    if (post_tag !== null) return post_tag;
+
+    return defaultCategory;
+}
+
+async function loadPostsData(url) {
+    try {
+        let postsData = await (await fetch(url)).json();
+
+        for (let i = 0; i < postsData.length; i++) {
+            let category = choseCategoryToShow(postsData[i]._embedded["wp:term"]);
+            let imageUrl = postsData[i].featured_media;
+            let postTitle = postsData[i].title.rendered;
+            let authorName = postsData[i]._embedded.author[0].name;
+            let authorProfileUrl = postsData[i]._embedded.author[0].link;             
+            let postUrl = postsData[i].link;
+
+            let postDate = new Date(postsData[i].date);
+
+            let datePosted = new Intl.DateTimeFormat(dateLocale, dateOptions).format(postDate)
+
+            addPost(category, imageUrl, postTitle, authorName, authorProfileUrl, datePosted, postUrl);
+        }
+    }
+    catch (error) {
+        console.error ("Error fetching posts data:", error);
+        document.getElementById("posts-container").innerHTML = "<p>Error fetching posts data</p>";        
+    }
+}
+
+loadPostsData(postsURL);
